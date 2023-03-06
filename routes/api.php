9e44854a04dd04c238d1ee50edfb37dab51927cb
Ye Yint Kyaw <yeyintkyaw300@gmail.com>
2023-03-06 15:17:12 +0630

    split auth routes with auth file

--- routes/api.php
+++ routes/api.php
@@ -8,17 +8,6 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\Auth\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -51,9 +40,4 @@ Route::prefix('/users')->middleware('auth:sactum')->group(function () {
     Route::apiResource('/', UserController::class);
 });
 
-// //auth routes
-Route::post('/register', [AuthController::class,'register']);
-Route::post('/login', [AuthController::class,'login']);
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class,'logout']);
-});
+require('auth.php');
